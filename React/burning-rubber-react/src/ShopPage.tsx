5189a83ff343d8eec618916d5297a441d4c71549
Saif Shinkar <saifshinkar@gmail.com>
2023-11-26 22:25:57 -0800

    converted to react format

--- React/burning-rubber-react/src/ShopPage.tsx
+++ React/burning-rubber-react/src/ShopPage.tsx
@@ -9,27 +9,27 @@ function ShopPage() {
       <form id="search-form">
         <div>
           <label>Search:</label>
-          <input type="text" id="searchBox" />
+          <input type="text" required placeholder='Insert Query Here' id="searchBox" />
         </div>
     
-        <input className="btn" type="submit" value="Search" />
+        <input className="btn" type="submit" value="Search" id="srchBtn" />
         <div className="message"></div>
       </form>
 
       <div id="allVehicles">
-        <h1 className="border-5">
+        <h1 className="border-5 d-flex align-items-center justify-content-center mt-3">
           All Vehicles
         </h1>
       </div>
 
       <div id="hilux">
         <p>
-          <img src="src/assets/toyota_hilux.jpg" className="d-block w-50 rounded center" alt="..." />
+          <img src="src/assets/toyota_hilux.jpg" className="d-block w-50 rounded centered" alt="..." />
         </p>
-        <h1>
+        <h1 className="d-flex align-items-center justify-content-center mt-3">
           Toyota Hilux
         </h1>
-        <p>
+        <p className="text-center mt-3">
           So indestructible that we guarantee a full refund if it breaks
           <br />
           300,000 miles
@@ -40,12 +40,12 @@ function ShopPage() {
 
       <div id="Lambo_R8">
         <p>
-          <img src="src/assets/lambo_tractor.jpg" className="d-block w-50 rounded center" alt="..." />
+          <img src="src/assets/lambo_tractor.jpg" className="d-block w-50 rounded centered" alt="..." />
         </p>
-        <h1>
+        <h1 className="d-flex align-items-center justify-content-center mt-3">
           Lamborghini R8
         </h1>
-        <p>
+        <p className="text-center mt-3">
           Not your typical Lamborghini
           <br />
           (Yes, Lamborghini actually made this)
@@ -56,12 +56,12 @@ function ShopPage() {
 
       <div id="Z8">
         <p>
-          <img src="src/assets/Z8.jpg" className="d-block w-50 rounded center" alt="..." />
+          <img src="src/assets/Z8.jpg" className="d-block w-50 rounded centered" alt="..." />
         </p>
-        <h1>
+        <h1 className="d-flex align-items-center justify-content-center mt-3">
           BMW Z8
         </h1>
-        <p>
+        <p className="text-center mt-3">
           2001 BMW naturally aspirated V8 roadster
           <br />
           15,000 miles
@@ -74,12 +74,12 @@ function ShopPage() {
 
       <div id="GTR">
         <p>
-          <img src="src/assets/GTR.jpg" className="d-block w-50 rounded center" alt="..." />
+          <img src="src/assets/GTR.jpg" className="d-block w-50 rounded centered" alt="..." />
         </p>
-        <h1>
+        <h1 className="d-flex align-items-center justify-content-center mt-3">
           Nissan GT-R
         </h1>
-        <p>
+        <p className="text-center mt-3">
           Godzilla
           <br />
           10,000 miles
@@ -92,12 +92,12 @@ function ShopPage() {
 
       <div id="autozam">
         <p>
-          <img src="src/assets/autozam.jpg" className="d-block w-50 rounded center" alt="..." />
+          <img src="src/assets/autozam.jpg" className="d-block w-50 rounded centered" alt="..." />
         </p>
-        <h1>
+        <h1 className="d-flex align-items-center justify-content-center mt-3">
           1993 Autozam AZ-1
         </h1>
-        <p>
+        <p className="text-center mt-3">
           JDM Gullwing kei car
           <br />
           35,000 miles
@@ -110,12 +110,12 @@ function ShopPage() {
 
       <div id="supra">
         <p>
-          <img src="src/assets/supra.jpg" className="d-block w-50 rounded center" alt="..." />
+          <img src="src/assets/supra.jpg" className="d-block w-50 rounded centered" alt="..." />
         </p>
-        <h1>
+        <h1 className="d-flex align-items-center justify-content-center mt-3">
           1997 Toyota Supra
         </h1>
-        <p>
+        <p className="text-center mt-3">
           IS THAT A SUPRA??
           <br />
           40,000 miles
