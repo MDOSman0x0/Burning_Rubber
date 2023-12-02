09617b70e389c379d06bf14fd37606ce944b52dc
jonarabo <arabojon@gmail.com>
2023-12-02 00:20:19 -0800

    Added search functionality to dealership

--- React/burning-rubber-react/src/ShopPage.tsx
+++ React/burning-rubber-react/src/ShopPage.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React from 'react';
 import Header from "./Header";
+import {FaSearch} from "react-icons/fa";
 import "./shopPage.css";
 
 class ShopPage extends React.Component {
   state = {
     details: [],
+    search: '',
   };
 
   componentDidMount() {
@@ -20,20 +22,34 @@ class ShopPage extends React.Component {
       .catch(err => { });
   }
 
+  handleSearchChange = (event) => {
+    this.setState({
+      search: event.target.value,
+    });
+  };
+
   render() {
+    const searchCars = this.state.details.filter(vehicle =>
+      vehicle.make.includes(this.state.search) || vehicle.type.includes(this.state.search)
+    );
+
     return (
       <div>
-        <Header />
+        <Header/>
+        <div className = 'searchBar'>
+          <FaSearch id="search-icon"/>
+        <input type='search' placeholder='Search make or model' value={this.state.search} onChange={this.handleSearchChange}/>
+        </div>
         <div className='container'>
           <div className='row'>
-            {this.state.details.map((output, id) => (
+            {searchCars.map((output, id) => (
               <div className='col-md-10 mb-5' key={id}>
                 <div className='card text-center'>
                   <img className= 'card-img-top' src={output['img']}/>
                   <div className='card-body'>
                     <h2 className='card-title'>{output['make']}</h2>
                     <h3 className='card-subtitle'>{output['type']}</h3>
-                    <a href='#'className='mt-1 btn btn-danger'>Add To Cart</a>
+                    <a href='#' className='mt-1 btn btn-danger'>Add To Cart</a>
                   </div>
                 </div>
               </div>
@@ -41,13 +57,11 @@ class ShopPage extends React.Component {
           </div>
         </div>
         <footer className="d-flex align-items-center justify-content-center mt-3 border-top position-static fixed-bottom">
-        {" "}
-        <p className="text-muted"> Copyright &copy; Burning Rubber 2023</p>{" "}
-      </footer>
+          <p className="text-muted"> Copyright &copy; Burning Rubber 2023</p>
+        </footer>
       </div>
     );
   }
 }
 
 export default ShopPage;
-
