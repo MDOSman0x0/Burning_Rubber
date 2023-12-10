63506c93322588b5b6cea5b8aa0f70d41cf6c374
jonarabo <arabojon@gmail.com>
2023-12-09 19:09:34 -0800

    Converted Reviews to PostgreSQL

--- reviews/reviews/settings.py
+++ reviews/reviews/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'reviews.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "reviews",
+        "USER": "postgres",
+        "PASSWORD": "123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
