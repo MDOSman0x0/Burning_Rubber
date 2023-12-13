bf0ac433fa5762428e3c9993b5f6836aecd1e74b
Luan <llam3967@sdsu.edu>
2023-12-12 22:14:11 -0800

    Reverted back to SQL_Lite

--- backend_users/user_backend/user_backend/settings.py
+++ backend_users/user_backend/user_backend/settings.py
@@ -79,14 +79,21 @@ WSGI_APPLICATION = 'user_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "users",
+#         "USER": "postgres",
+#         "PASSWORD": "123",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "users",
-        "USER": "postgres",
-        "PASSWORD": "123",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- reviews/reviews/settings.py
+++ reviews/reviews/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'reviews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "reviews",
+#         "USER": "postgres",
+#         "PASSWORD": "123",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "reviews",
-        "USER": "postgres",
-        "PASSWORD": "123",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
